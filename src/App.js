4e056c9b7bba8821584b4274fba62bd465d41253
Viacheslav Myrhorod <slavik.mirgorod@gmail.com>
2023-02-23 19:13:17 +0200

    html code for main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
-    <div className="App">
-      <h3>Text</h3>
+    <div className='all'>
+      <div className='hdr'>
+      
+      </div>
+      <div className='main'>
+        <div className='nav'>
+          <ul>
+            <li><a>Profile</a></li>
+            <li><a>Messages</a></li>
+            <li><a>News</a></li>
+            <li><a>Music</a></li>
+            <li><a>Settings</a></li>
+          </ul>
+        </div>
+        <div className='body'>
+          <img src='' />
+          <div className='profileInfo'>
+            <img src='' />
+            <div>
+            
+            </div>
+          </div>
+          <div className='writePost'>
+            <p>My posts</p>
+            <input />
+            <input />
+          </div>
+          <div className='posts'>
+          
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
